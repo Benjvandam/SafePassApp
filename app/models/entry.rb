698b710dbc7d6e0f2a54dadd73ae053fce050ee9
Benjamin Van Dam <benjamin.van.dam@outlook.com>
2025-09-03 23:06:47 +0200

    Create Entry resource

---
+++ app/models/entry.rb
@@ -0,0 +1,17 @@
+class Entry < ApplicationRecord
+  belongs_to :user
+
+  validates :name, :username, :password, presence: true
+  validate :url_must_be_valid
+
+  encrypts :username, deterministic: true
+  encrypts :password
+
+  private
+
+  def url_must_be_valid
+    unless url.include?("http" || "https")
+      errors.add(:url, "must start with http")
+    end
+  end
+end
